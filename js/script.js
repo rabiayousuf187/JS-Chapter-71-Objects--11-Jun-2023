4c6d724b83bcec4b4b5efcce5b318f96896e36b5
Rabia Yousuf <53699291+rabiayousuf187@users.noreply.github.com>
2023-06-11 09:54:02 +0500

    Obj add and delete parameter

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
 console.log("JS Chapter 69 Objects");
+
+let obj = {
+    name: "John",
+    p_no: 1245,
+    age: 22,
+};
+
+console.log("Object ==== ", obj);
+console.log("Object type ==== ", typeof obj);
+obj.qualification = "Grduated";
+console.log("Object after add parameter ==== ", obj);
+delete obj.age;
+console.log("Object delete parameter ==== ", obj);
+
+
+
