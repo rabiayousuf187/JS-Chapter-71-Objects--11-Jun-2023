5b97eb53b6e379c07d0118ecedef29236b6ff608
Rabia Yousuf <53699291+rabiayousuf187@users.noreply.github.com>
2023-06-11 10:12:18 +0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,17 +1,30 @@
-console.log("JS Chapter 69 Objects");
+console.log("JS Chapter 69 stdects");
 
-let obj = {
+let std = {
     name: "John",
     p_no: 1245,
     age: 22,
+    status: "not-registered",
+    statusStd: function(update){
+        console.log("Entererd in STD Method");
+        this.status = update;
+        console.log("this.std.status updated");
+        
+        return std.status;
+    }
 };
 
-console.log("Object ==== ", obj);
-console.log("Object type ==== ", typeof obj);
-obj.qualification = "Grduated";
-console.log("Object after add parameter ==== ", obj);
-delete obj.age;
-console.log("Object delete parameter ==== ", obj);
+console.log("stdect ==== ", std);
+console.log("stdect type ==== ", typeof std);
+std.qualification = "Grduated";
+console.log("Object after add parameter ==== ", std);
+delete std.age;
+console.log("Object delete parameter ==== ", std);
+
+console.log("Student status has been updated to", std.statusStd("registered"));
+
+
+
 
 
 
